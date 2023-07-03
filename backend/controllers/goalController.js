3580564b31a9e653b345c9306267e37fd9915db2
GUchechi <godswilluchendu7@gmail.com>
2023-07-03 19:50:32 +0100

    setPost

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -17,9 +17,14 @@ const getGoals = asyncHandler(async (req, res) => {
 
 const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
+    res.status(400);
     throw new Error("Please add a text field");
   }
-  res.status(200).json({ message: "Set goals" });
+
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+  res.status(200).json(goal);
 });
 
 // @desc Update goal
