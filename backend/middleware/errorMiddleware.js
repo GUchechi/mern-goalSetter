1d8ea4a0e0aca218a523ad4f326560648b4cc306
GUchechi <godswilluchendu7@gmail.com>
2023-07-03 18:02:35 +0100

    err handler

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -3,7 +3,7 @@ const errorHandler = (err, req, res, next) => {
 
   res.status(statusCode);
 
-  res.join({
+  res.json({
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
