2d309dde836136fab5919a63a37a28ac59dab71d
GUchechi <godswilluchendu7@gmail.com>
2023-07-03 16:44:31 +0100

    routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,5 +1,10 @@
 const express = require("express");
-const { getGoals } = require("../controllers/goalController");
+const {
+  getGoal,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
 const router = express.Router();
 
 router.get("/", getGoals);
