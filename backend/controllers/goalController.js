7a14a814b2180ccb52917e49cf928336214c3902
GUchechi <godswilluchendu7@gmail.com>
2023-07-03 16:44:05 +0100

    routes

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,7 +1,38 @@
-const getGoals = (req, res) => {
+// @desc Get goals
+// @route GET /api/goal
+// @access Private
+
+const getGoal = (req, res) => {
   res.status(200).json({ message: "Get goals" });
 };
 
+// @desc Set goals
+// @route POST /api/goal
+// @access Private
+
+const setGoal = (req, res) => {
+  res.status(200).json({ message: "Set goals" });
+};
+
+// @desc Update goal
+// @route PUT /api/goal/:id
+// @access Private
+
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: "Update goals" });
+};
+
+// @desc Delete goals
+// @route DELETE /api/goal/:id
+// @access Private
+
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: "Delete goals" });
+};
+
 module.exports = {
-  getGoals,
+  getGoal,
+  setGoal,
+  updateGoal,
+  deleteGoal,
 };
