e4cc8bab9c155d274813819474fe2794cd4202fa
GUchechi <godswilluchendu7@gmail.com>
2023-07-05 13:54:42 +0100

    registerUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,18 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+
 // @desc    Register new user
 // @route   POST /api/users
 // @access  Public
-const registerUser = (req, res) => {
-  res.json({ message: "Register User" });
-};
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+});
 
 // @desc    Authenticate a user
 // @route   POST /api/users/login
