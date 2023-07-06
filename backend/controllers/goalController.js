24f2a0ba4dadf201ce41d34c9f21cb401a1629aa
GUchechi <godswilluchendu7@gmail.com>
2023-07-06 14:44:43 +0100

    login autentication

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 
 const Goal = require("../model/goalModel");
+const User = require("../model/userModel");
 
 // @desc Get goals
 // @route GET /api/goal
@@ -36,6 +37,20 @@ const updateGoal = asyncHandler(async (req, res) => {
   if (!goal) {
     res.status(400);
     throw new Error("Goal not found");
+  }
+
+  const user = await User.findById(req.user.id);
+
+  // Check for User
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  // Make sure the logged in user matches in goal
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
   }
 
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
