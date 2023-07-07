b157c8358e5df096ec6bc243081d878369ab23fe
GUchechi <godswilluchendu7@gmail.com>
2023-07-07 01:22:27 +0100

    added registerPage

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,14 +1,26 @@
 import { useState, useEffect } from "react";
+import { FaUser } from "react-icons/fa";
 
 const Register = () => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     password: "",
-    password2: "",
+    confirmPassword: "",
   });
 
-  return <div>Register</div>;
+  const { name, email, password, confirmPassword } = formData;
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please Create an Account</p>
+      </section>
+    </>
+  );
 };
 
 export default Register;
