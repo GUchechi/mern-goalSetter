4e6d4653ceacfcf6777f0a60d6af6c54f418d230
GUchechi <godswilluchendu7@gmail.com>
2023-07-07 03:26:02 +0100

    logout

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -13,9 +13,15 @@ const register = async (userData) => {
   return response.data;
 };
 
+
+// Logout user
+const logout = () => {
+    localStorage.removeItem('user')
+  }
+  
 const authService = {
   register,
-//   logout,
+  logout,
 //   login,
 };
 
