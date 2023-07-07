66ec79823a40c9a56e6c71cfa82a09d1e3cb7e4a
GUchechi <godswilluchendu7@gmail.com>
2023-07-07 01:46:18 +0100

    logic

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -3,3 +3,11 @@ import authService from "./authService";
 
 // Get user from localStorage
 const user = JSON.parse(localStorage.getItem("user"));
+
+const initialState = {
+  user: user ? user : null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
