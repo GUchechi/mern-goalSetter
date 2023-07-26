ad2fda9739a9c65ff4aa04e4d59ff52140d21ea8
GUchechi <godswilluchendu7@gmail.com>
2023-07-26 13:43:38 +0100

    goals

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,18 +1,30 @@
-import { useDispatch } from 'react-redux'
-import { deleteGoal } from '../features/goals/goalSlice'
+import { useDispatch } from "react-redux";
+import { deleteGoal } from "../features/goals/goalSlice";
 
 function GoalItem({ goal }) {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+
+  // Function to format the date to "MMM DD h:mmA" format
+  const formatDateTime = (dateTimeString) => {
+    const options = {
+      month: "short", // Short month name (e.g., Jul)
+      day: "numeric", // Numeric day (e.g., 26)
+      hour: "numeric", // Numeric hour (e.g., 1)
+      minute: "numeric", // Numeric minute (e.g., 18)
+      hour12: true, // 12-hour format (e.g., 1:18pm)
+    };
+    return new Date(dateTimeString).toLocaleString("en-US", options);
+  };
 
   return (
-    <div className='goal'>
-      <div>{new Date(goal.createdAt).toLocaleString('en-US')}</div>
+    <div className="goal">
+      <div>{formatDateTime(goal.createdAt)}</div>
       <h2>{goal.text}</h2>
-      <button onClick={() => dispatch(deleteGoal(goal._id))} className='close'>
+      <button onClick={() => dispatch(deleteGoal(goal._id))} className="close">
         X
       </button>
     </div>
-  )
+  );
 }
 
-export default GoalItem
+export default GoalItem;
