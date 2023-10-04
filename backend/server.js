7f2c501e6e38d84338704963504b85d85345aab8
GUchechi <godswilluchendu7@gmail.com>
2023-10-04 11:03:47 +0100

    added frontend register functionality

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,12 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const { connectDB } = require("./config/db");
 
 dotenv.config();
-
+ 
 const port = process.env.PORT || 5000;
 
 connectDB();
 
-const app = express(); 
+const app = express();  
  
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
